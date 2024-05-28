3003c007b1e4d57412315b7f51cfd8f85981af7e
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-05-27 23:19:51 -0300

    add actual elements to the solution instead of its positions

--- src/App.java
+++ src/App.java
@@ -1,18 +1,16 @@
-import java.util.Arrays;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         int[] numbers = {10, 20, 30, 40, 14, 22};
 
-        List<Boolean[]> solutions = SubsetSum.calculate(numbers, 30);     
-        
-        System.out.printf("Sorted list: %s\n", Arrays.toString(Arrays.stream(numbers).sorted().toArray()));
-        System.out.println("Results: ");
+        List<List<Integer>> solutions = SubsetSum.calculate(numbers, 30);
+
+        System.out.println("Solutions found: ");
         int counter = 0;
-        for (Boolean[] solution : solutions) {
+        for (List<Integer> solution : solutions) {
             counter++;
-            System.out.printf("\t%d. %s\n", counter, Arrays.toString(solution));
+            System.out.printf("\t%d. %s\n", counter, solution);
         }
     }
 }
--- src/SubsetSum.java
+++ src/SubsetSum.java
@@ -5,13 +5,12 @@ import java.util.List;
 public class SubsetSum {
     /**
      * Solves the Subset Sum problem.
-     * @param numbers the set of numbers to investigate.
+     * @param numbers the list of numbers to investigate.
      * @param desiredSum the sum desired to be found in {@code numbers}.
-     * @return a list containing {@code false} at the positions of elements not included in the solution and {@code true} at the positions of elements included, relative to the
-     * SORTED {@code numbers} list.
+     * @return a list of lists of integers where each list represents a possible set of numbers in {@code numbers} that add up to {@code desiredSum}.
      */
-    public static List<Boolean[]> calculate(int[] numbers, int desiredSum) {
-        List<Boolean[]> solutions = new LinkedList<>();
+    public static List<List<Integer>> calculate(int[] numbers, int desiredSum) {
+        List<List<Integer>> solutions = new LinkedList<>();
 
         // sort original array
         if (numbers == null || numbers.length == 0) return solutions;
@@ -26,13 +25,27 @@ public class SubsetSum {
         return solutions;
     }
 
-    public static void sumOfSubset(int[] numbers, int desiredSum, int sumOfAnalyzedItems, int currentItemIdx, int sumOfLeftItems, Boolean[] currentSolution, List<Boolean[]> solutions) {
+    /**
+     * Effectively calculates the Subset Sum problem using backtracking.
+     * @param numbers the list of numbers to investigate.
+     * @param desiredSum the sum desired to be found in {@code numbers}.
+     * @param sumOfAnalyzedItems the current sum of all items that have already been analyzed.
+     * @param currentItemIdx the index of the current element in {@code numbers} being investigated.
+     * @param sumOfLeftItems the sum of all items that still haven't been investigated.
+     * @param currentSolution the current solution being built.
+     * @param solutions all solutions currently found.
+     */
+    public static void sumOfSubset(int[] numbers, int desiredSum, int sumOfAnalyzedItems, int currentItemIdx, int sumOfLeftItems, Boolean[] currentSolution, List<List<Integer>> solutions) {
         // generate left child
         currentSolution[currentItemIdx] = true;
         if (sumOfAnalyzedItems+numbers[currentItemIdx] == desiredSum) { // solution has been found
-            for (int i=currentItemIdx+1; i<currentSolution.length; i++) // fill with 'false' the remaining positions (previously 'null')
-                currentSolution[i] = false;
-            solutions.add(Arrays.copyOf(currentSolution, currentSolution.length));
+            // create a new list with the numbers in this solution and add it to the solutions list
+            List<Integer> list = new LinkedList<>();
+            for (int i=0; i<numbers.length; i++) {
+                if (currentSolution[i] == null) break; // from now on numbers have not been investigated
+                if (currentSolution[i]) list.add(numbers[i]);
+            }
+            solutions.add(list);
         }
         else if (sumOfAnalyzedItems+numbers[currentItemIdx]+numbers[currentItemIdx+1] <= desiredSum) // bounding function (check expansion to the left)
             sumOfSubset(numbers, desiredSum, sumOfAnalyzedItems+numbers[currentItemIdx], currentItemIdx+1, sumOfLeftItems-numbers[currentItemIdx], currentSolution, solutions);
